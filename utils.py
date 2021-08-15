cbc37192359859ca9c886e1091079ae06defae92
Gustavo Ramirez <g.ramirez@stimulate-ejd.eu>
2021-08-15 13:28:25 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,12 +15,14 @@ def flopsV(nr_levels, levels_info, level_id):
     if nr_levels == 1:
         return levels_info[0].A.nnz
     elif level_id == nr_levels-2:
-        return 2 * levels_info[level_id].A.nnz + levels_info[level_id+1].A.nnz
+        #FIXME
+        return (2+2) * levels_info[level_id].A.nnz + levels_info[level_id+1].A.nnz
     else:
+        # FIXME
         if level_id==0:
-            return (2+1) * levels_info[level_id].A.nnz + flopsV(nr_levels, levels_info, level_id+1)
+            return (2+2) * levels_info[level_id].A.nnz + flopsV(nr_levels, levels_info, level_id+1)
         else:
-            return 2 * levels_info[level_id].A.nnz + flopsV(nr_levels, levels_info, level_id+1)
+            return (2+2) * levels_info[level_id].A.nnz + flopsV(nr_levels, levels_info, level_id+1)
 
 # adds the Krylov that wraps the AMG solver
 def flopsV_manual(bare_level, levels_info, level_id):
@@ -32,11 +34,11 @@ def flopsV_manual(bare_level, levels_info, level_id):
         # FIXME : number 30 hardcoded
         #return 2 * mg.smoother_iters * levels_info[level_id].A.nnz + 30*levels_info[level_id+1].A.nnz
         if level_id==bare_level:
-            return (2+1) * mg.smoother_iters * levels_info[level_id].A.nnz + 0
+            return (2 * mg.smoother_iters + 2) * levels_info[level_id].A.nnz + 0
         else:
-            return 2 * mg.smoother_iters * levels_info[level_id].A.nnz + 0
+            return (2 * mg.smoother_iters + 1) * levels_info[level_id].A.nnz + 0
     else:
         if level_id==bare_level:
-            return (2+1) * mg.smoother_iters * levels_info[level_id].A.nnz + flopsV_manual(bare_level, levels_info, level_id+1)
+            return (2 * mg.smoother_iters + 2) * levels_info[level_id].A.nnz + flopsV_manual(bare_level, levels_info, level_id+1)
         else:
-            return 2 * mg.smoother_iters * levels_info[level_id].A.nnz + flopsV_manual(bare_level, levels_info, level_id+1)
+            return (2 * mg.smoother_iters + 1) * levels_info[level_id].A.nnz + flopsV_manual(bare_level, levels_info, level_id+1)
